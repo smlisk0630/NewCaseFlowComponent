10e692f3fa438491c211d13465859a5ba6e1937e
slisk <63676970+smlisk0630@users.noreply.github.com>
2020-11-03 15:04:49 -0500

    Updates to component, style, and controller files

--- force-app/main/default/aura/NewCaseFlow/NewCaseFlowController.js
+++ force-app/main/default/aura/NewCaseFlow/NewCaseFlowController.js
@@ -2,7 +2,7 @@
     init : function(component) {
         // Find the component whose aura:id is "flowData"
         let flow = component.find("flowData");
-        // In that component, start your flow. Reference the flow's Unique Name/API Name.THIS
+        // In that component, start your flow. Reference the flow's Unique Name/API Name.
     flow.startFlow("New_Case");
     }
 })
